4c473b8feba2e47983c7d41146ab1ad50e132087
Katrina_Dierking <kalynndier@gmail.com>
2019-12-08 21:22:54 -0500

    initial commit. All installs loaded

---
+++ friends/src/components/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} with 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
+class Login extends Component {
+    state = {
+        credentials : {
+            email: '',
+            password: ''
+        }
+    };
+
+    
+}
