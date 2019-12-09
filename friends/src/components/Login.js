b5fa9b42f884aee7f9a8855f7d686a0203b9a8b6
Katrina_Dierking <kalynndier@gmail.com>
2019-12-08 21:38:59 -0500

    Login page initialized.

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -9,5 +9,51 @@ class Login extends Component {
         }
     };
 
-    
+  handleChange = (e) => {
+      this.setState ({
+          credentials: {
+              ...this.state.credentials,
+              [e.target.name] : e.target.value
+          }
+      });
+  };
+  
+  login = (e) => {
+      e.preventDefault();
+      axiosWithAuth()
+      .post('/login', this.state.credentials)
+      .then (result => {
+          localStorage.setItem('token', res.data.payload);
+          this.props.history.push('/protected');
+      })
+      .catch(error => console.log(error));
+  };
+
+  render () {
+      return (
+          <>
+          <form onSubmit = {this.login}>
+                <input 
+                    type = 'text'
+                    name = 'email'
+                    placeholder = 'email'
+                    value = {this.state.credentials.email}
+                    onChange = {this.handleChange}
+                    />
+
+                <input 
+                    type = 'password'
+                    name = 'password'
+                    placeholder = ' * password'
+                    value = {this.state.credentials.password}
+                    onChange = {this.handleChange}
+                    />
+
+                    <button> Log In </button>
+            </form>
+        </>
+      )
+  }
 }
+
+export default Login;
